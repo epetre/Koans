c98448baa737fb9e3e7aef93d5f5412578ef731b
Emanuel Petre <petreemanuel@gmail.com>
2011-11-14 13:53:55 -0500

    149

--- koans/about_triangle_project.rb
+++ koans/about_triangle_project.rb
@@ -3,7 +3,15 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 # You need to write the triangle method in the file 'triangle.rb'
 require 'triangle.rb'
 
+
+
 class AboutTriangleProject < EdgeCase::Koan
+  
+  #def triangle(left, top, right) do
+    #puts (left == top == right).inspect
+    #:equilateral if (left == top == right)
+  #end
+  
   def test_equilateral_triangles_have_equal_sides
     assert_equal :equilateral, triangle(2, 2, 2)
     assert_equal :equilateral, triangle(10, 10, 10)
