431bc0b2089977d4da997f92b26d6c79900b0da6
Emanuel Petre <petreemanuel@gmail.com>
2012-01-20 13:08:29 -0500

    256.

--- koans/about_message_passing.rb
+++ koans/about_message_passing.rb
@@ -56,11 +56,11 @@ class AboutMessagePassing < EdgeCase::Koan
   def test_sending_a_message_with_arguments
     mc = MessageCatcher.new
 
-    assert_equal __, mc.add_a_payload
-    assert_equal __, mc.send(:add_a_payload)
+    assert_equal [], mc.add_a_payload
+    assert_equal [], mc.send(:add_a_payload)
 
-    assert_equal __, mc.add_a_payload(3, 4, nil, 6)
-    assert_equal __, mc.send(:add_a_payload, 3, 4, nil, 6)
+    assert_equal [3,4,nil,6], mc.add_a_payload(3, 4, nil, 6)
+    assert_equal [3,4,nil,6], mc.send(:add_a_payload, 3, 4, nil, 6)
   end
 
   # ------------------------------------------------------------------
@@ -71,7 +71,7 @@ class AboutMessagePassing < EdgeCase::Koan
   def test_sending_undefined_messages_to_a_typical_object_results_in_errors
     typical = TypicalObject.new
 
-    exception = assert_raise(___) do
+    exception = assert_raise(NoMethodError) do
       typical.foobar
     end
     assert_match(/foobar/, exception.message)
@@ -80,7 +80,7 @@ class AboutMessagePassing < EdgeCase::Koan
   def test_calling_method_missing_causes_the_no_method_error
     typical = TypicalObject.new
 
-    exception = assert_raise(___) do
+    exception = assert_raise(NoMethodError) do
       typical.method_missing(:foobar)
     end
     assert_match(/foobar/, exception.message)
@@ -114,9 +114,9 @@ class AboutMessagePassing < EdgeCase::Koan
   def test_all_messages_are_caught
     catcher = AllMessageCatcher.new
 
-    assert_equal __, catcher.foobar
-    assert_equal __, catcher.foobaz(1)
-    assert_equal __, catcher.sum(1,2,3,4,5,6)
+    assert_equal "Someone called foobar with <>", catcher.foobar
+    assert_equal "Someone called foobaz with <1>", catcher.foobaz(1)
+    assert_equal "Someone called sum with <1, 2, 3, 4, 5, 6>", catcher.sum(1,2,3,4,5,6)
   end
 
   def test_catching_messages_makes_respond_to_lie
@@ -125,7 +125,7 @@ class AboutMessagePassing < EdgeCase::Koan
     assert_nothing_raised(NoMethodError) do
       catcher.any_method
     end
-    assert_equal __, catcher.respond_to?(:any_method)
+    assert_equal false, catcher.respond_to?(:any_method)
   end
 
   # ------------------------------------------------------------------
